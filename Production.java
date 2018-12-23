29858906c0ad145f03ef04e4cfc45c569de238e7
snkr10 <46087733+snkr10@users.noreply.github.com>
2018-12-24 03:12:08 +0900

    データ受け渡しjava

---
+++ Production.java
@@ -0,0 +1,67 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebServlet("/Production")
+public class Production extends HttpServlet implements ServletContextListener {
+	private static final long serialVersionUID = 1L;
+
+	public Production() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		BufferedReader buffer = new BufferedReader(request.getReader());
+		String reqJson = buffer.readLine();
+
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, String> reqMap = mapper.readValue(reqJson, new TypeReference<Map<String, String>>() {
+		});
+
+		List<String> list = new ArrayList<>();
+		list.add(reqMap.get("str1"));
+
+		Map<String, Object> resMap = new HashMap<>();
+
+		resMap.put("ret", "true");
+		resMap.put("ary", list);
+
+		String resJson = mapper.writeValueAsString(resMap);
+
+		response.setContentType("application/json");
+		response.setHeader("Cache-Control", "nocache");
+		response.setCharacterEncoding("utf-8");
+
+		PrintWriter out = response.getWriter();
+		out.print(resJson);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		System.out.println("contextDestroyed");
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		System.out.println("contextInitialized");
+	}
+
+}
